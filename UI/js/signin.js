e418ad42b34d6f0cf7186c3982b17f2b67bd8187
Marshalheri <chizyberto@gmail.com>
2019-05-17 08:24:51 -0700

    feature(signin-page): update the html and js files to handle navigation to home page on login [Fixes #165980870]

--- UI/js/signin.js
+++ UI/js/signin.js
@@ -1,3 +1,5 @@
+
+//This section handles the toggle between the admin login form and the user login form...
 let userBtn = document.getElementById("toggleUserBtn");
 let adminBtn = document.getElementById("toggleAdminBtn");
 let userForm = document.getElementById("userLoginForm");
@@ -16,3 +18,37 @@ adminBtn.onclick = (e) =>{
 	userForm.style.display = "none";
 	adminForm.style.display = "flex";
 }
+
+//The toggle handler ends here........
+
+
+//This section handles the user login....
+let loginUser = document.getElementById("loginUserBtn");
+
+loginUser.addEventListener("click", (e) =>{
+	let userEmail = document.getElementById("userEmail").value;
+	let userPassword = document.getElementById("userPassword").value;
+	if(userEmail == "" || userPassword == ""){
+		//add some functions to validate here later...
+	}
+	else{
+	let currentUrl = location;
+	let redirectUrl = currentUrl.protocol + currentUrl.host + "/C:/Users/Marshalheri/Documents/Auto-Mart/UI/home.html"
+	window.location.assign(redirectUrl);}
+})
+
+//This section handles the admin login
+let loginAdmin = document.getElementById("loginAdminBtn");
+
+loginAdmin.addEventListener("click", (e) =>{
+	let adminEmail = document.getElementById("adminEmail").value;
+	let adminPassword = document.getElementById("adminPassword").value;
+	if(adminEmail == "" || adminPassword == ""){
+		//add some functions here later....
+	}else{
+		let currentUrl = location;
+		let redirectUrl = currentUrl.protocol + currentUrl.host + "/C:/Users/Marshalheri/Documents/Auto-Mart/UI/admin-dashboard.html"
+		
+		window.location.assign(redirectUrl);
+	}
+})
