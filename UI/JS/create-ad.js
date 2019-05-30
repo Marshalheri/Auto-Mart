3d5f2c46b2e47090a38edb297c8d188f4969f902
Marshalheri <chizyberto@gmail.com>
2019-05-30 10:55:18 -0700

    bug(user-login):update after use of lint [Fixes #165980870]

--- UI/JS/create-ad.js
+++ UI/JS/create-ad.js
@@ -1,9 +1,17 @@
 
-let createAd = document.getElementById("createAdBtn");
+const createAd = document.getElementById('createAdBtn');
 
+<<<<<<< Updated upstream
 createAd.addEventListener("click", (e) =>{
 
 	let currentUrl = location;
 	let redirectUrl = currentUrl.protocol + "//" + currentUrl.host + "/Auto-Mart/UI/home.html";
 	window.location.assign(redirectUrl);
 })
+=======
+createAd.addEventListener('click', (e) => {
+  const currentUrl = location;
+  const redirectUrl = `${currentUrl.protocol}//${currentUrl.host}/Auto-Mart/UI/home.html`;
+  window.location.assign(redirectUrl);
+});
+>>>>>>> Stashed changes
--- UI/JS/create-order.js
+++ UI/JS/create-order.js
@@ -1,9 +1,9 @@
 
-let orderBtn = document.getElementById("placeOrderBtn");
+const orderBtn = document.getElementById('placeOrderBtn');
 
-orderBtn.addEventListener("click", (e) =>{
-	e.preventDefault();
-	let url = location;
-	let newUrl = url.origin + " " + url.pathname;
-	window.alert(newUrl);
-})
+orderBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const url = location;
+  const newUrl = `${url.origin} ${url.pathname}`;
+  window.alert(newUrl);
+});
